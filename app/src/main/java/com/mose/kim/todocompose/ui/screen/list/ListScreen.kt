28b9f2510698dec3057c85315b4934ed1b2ceb2d
Mose <km_413@naver.com>
2023-01-18 00:21:59 +0900

    Display All Tasks - Observe Database

--- app/src/main/java/com/mose/kim/todocompose/ui/screen/list/ListScreen.kt
+++ app/src/main/java/com/mose/kim/todocompose/ui/screen/list/ListScreen.kt
@@ -1,6 +1,7 @@
 package com.mose.kim.todocompose.ui.screen.list
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -8,6 +9,8 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -23,7 +26,14 @@ fun ListScreen(
     navigationToTaskScreen: (Int) -> Unit,
     sharedViewModel: SharedViewModel
 ) {
+    // LaunchedEfffect - 컴포즤션이 시작되면 조건(key..)에 맞게 실행되며 범위 내에서 안전하게 종료
+    // key 값에 state가 들어가 상태에 따라 트리거로 작동시킴
+    LaunchedEffect(key1 = true) {
+        sharedViewModel.getAllTasks()
+    }
 
+    // collectAsState를 통해서 allTasks의 값들을 최신상태로 유지
+    val allTasks by sharedViewModel.allTask.collectAsState() // collectAsState - StateFlow를 통해 값을 가져오고 State를 통해 최신의 데이터를 가져옴
     //
     val searchAppBarState: SearchAppBarState by sharedViewModel.searchAppBarState
     val searchTextState: String by sharedViewModel.searchTextState
@@ -38,7 +48,10 @@ fun ListScreen(
             )
         },
         content = {
-                  ListContent()
+                  ListContent(
+                      allTasks,
+                      navigationToTaskScreen
+                  )
         },
         floatingActionButton = {
             ListFab(navigationToTaskScreen = navigationToTaskScreen)
