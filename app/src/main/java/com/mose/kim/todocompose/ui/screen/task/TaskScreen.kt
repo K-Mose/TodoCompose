d4e4ad7e6e8b2c002b0267b5e458b94243403c9d
Mose <km_413@naver.com>
2023-01-27 16:06:31 +0900

    Task Screen - Task Content

--- app/src/main/java/com/mose/kim/todocompose/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/com/mose/kim/todocompose/ui/screen/task/TaskScreen.kt
@@ -1,7 +1,12 @@
 package com.mose.kim.todocompose.ui.screen.task
 
+import android.util.Log
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import com.mose.kim.todocompose.components.PriorityDropDown
+import com.mose.kim.todocompose.data.model.Priority
 import com.mose.kim.todocompose.data.model.ToDoTask
 import com.mose.kim.todocompose.util.Action
 
@@ -19,6 +24,17 @@ fun TaskScreen(
         },
         content = {
             it
+            var tempPriority by remember {
+                mutableStateOf(selectedTask?.priority ?: Priority.NONE)
+            }
+            TaskContent(
+                title = selectedTask?.title ?: "",
+                onTitleChange = {},
+                description = selectedTask?.description ?: "",
+                onDescriptionChange = {},
+                priority = tempPriority,
+                onPrioritySelected = {}
+            )
         }
     )
 }
